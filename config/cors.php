920c6cdaccc6d0d4e51d23ce5573587398c198ba
Debjit Biswas <debjit012@gmail.com>
2022-04-24 21:22:56 +0530

    added-all-route-CROS

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['*','sanctum/csrf-cookie','api/*'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
@@ -30,6 +30,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
